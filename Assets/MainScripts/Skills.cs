e0dba174169c6bbb5cd84d8096d15f4232a15c0d
Rezivel <46860998+Rezivel@users.noreply.github.com>
2019-07-07 10:39:01 +0300

    removed address from testnet

--- Assets/MainScripts/Skills.cs
+++ Assets/MainScripts/Skills.cs
@@ -11,11 +11,11 @@ public class Skills : MonoBehaviour {
     void Awake()
     {
         DontDestroyOnLoad(gameObject);
-        Dictionary<string,object> a = new Node(Node.TestNetHost).GetAddressData("3N9L8LAAPnzqfw6Cm3fDJ4jcFX5QM2ht6Ct");
-        //string path = Application.streamingAssetsPath + "/Skills.json";
-        //string JsonSkills = File.ReadAllText(path);
-        //skills = JsonConvert.DeserializeObject<List<Skill>>(JsonSkills);
-        skills = JsonConvert.DeserializeObject<List<Skill>>(Encoding.UTF8.GetString(Base58.Decode(a.GetValue("skills").ToString())));
+        //Dictionary<string,object> a = new Node(Node.TestNetHost).GetAddressData("3N9L8LAAPnzqfw6Cm3fDJ4jcFX5QM2ht6Ct");
+        string path = Application.streamingAssetsPath + "/Skills.json";
+        string JsonSkills = File.ReadAllText(path);
+        skills = JsonConvert.DeserializeObject<List<Skill>>(JsonSkills);
+        //skills = JsonConvert.DeserializeObject<List<Skill>>(Encoding.UTF8.GetString(Base58.Decode(a.GetValue("skills").ToString())));
     }
 
 }
